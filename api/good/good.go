90d11e708dece73199f02e887fccb3def471722b
Zhao KK <kikakkz@hotmail.com>
2022-10-11 14:24:30 +0000

    Add update good

--- api/good/good.go
+++ api/good/good.go
@@ -26,6 +26,7 @@ import (
 	"github.com/google/uuid"
 )
 
+// nolint
 func (s *Server) CreateGood(ctx context.Context, in *npool.CreateGoodRequest) (*npool.CreateGoodResponse, error) {
 	var err error
 
@@ -58,6 +59,24 @@ func (s *Server) CreateGood(ctx context.Context, in *npool.CreateGoodRequest) (*
 		return &npool.CreateGoodResponse{}, status.Error(codes.InvalidArgument, "Price is invalid")
 	}
 
+	switch in.GetInfo().GetBenefitType() {
+	case mgrpb.BenefitType_BenefitTypePlatform:
+	case mgrpb.BenefitType_BenefitTypePool:
+	default:
+		logger.Sugar().Errorw("CreateGood", "BenefitType", in.GetInfo().GetBenefitType())
+		return &npool.CreateGoodResponse{}, status.Error(codes.InvalidArgument, "BenefitType is invalid")
+	}
+
+	switch in.GetInfo().GetGoodType() {
+	case mgrpb.GoodType_GoodTypeClassicMining:
+	case mgrpb.GoodType_GoodTypeUnionMining:
+	case mgrpb.GoodType_GoodTypeTechniqueFee:
+	case mgrpb.GoodType_GoodTypeElectricityFee:
+	default:
+		logger.Sugar().Errorw("CreateGood", "GoodType", in.GetInfo().GetGoodType())
+		return &npool.CreateGoodResponse{}, status.Error(codes.InvalidArgument, "GoodType is invalid")
+	}
+
 	if in.GetInfo().GetTitle() == "" {
 		logger.Sugar().Errorw("CreateGood", "Title", in.GetInfo().GetTitle())
 		return &npool.CreateGoodResponse{}, status.Error(codes.InvalidArgument, "Title is invalid")
@@ -84,6 +103,11 @@ func (s *Server) CreateGood(ctx context.Context, in *npool.CreateGoodRequest) (*
 	if in.GetInfo().GetStartAt() <= now {
 		logger.Sugar().Errorw("CreateGood", "StartAt", in.GetInfo().GetStartAt(), "now", now)
 		return &npool.CreateGoodResponse{}, status.Error(codes.InvalidArgument, "StartAt is invalid")
+	}
+
+	if in.GetInfo().GetTotal() <= 0 {
+		logger.Sugar().Errorw("CreateGood", "Total", in.GetInfo().GetTotal())
+		return &npool.CreateGoodResponse{}, status.Error(codes.InvalidArgument, "Total is invalid")
 	}
 
 	span = commontracer.TraceInvoker(span, "Good", "mw", "CreateGood")
@@ -213,6 +237,147 @@ func (s *Server) GetGoods(ctx context.Context, in *npool.GetGoodsRequest) (*npoo
 	}, nil
 }
 
+// nolint
 func (s *Server) UpdateGood(ctx context.Context, in *npool.UpdateGoodRequest) (*npool.UpdateGoodResponse, error) {
-	return &npool.UpdateGoodResponse{}, nil
+	var err error
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "UpdateGood")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(scodes.Error, err.Error())
+			span.RecordError(err)
+		}
+	}()
+
+	if _, err := uuid.Parse(in.GetInfo().GetID()); err != nil {
+		logger.Sugar().Errorw("UpdateGood", "ID", in.GetInfo().GetID(), "error", err)
+		return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	if in.GetInfo().DeviceInfoID != nil {
+		if _, err := uuid.Parse(in.GetInfo().GetDeviceInfoID()); err != nil {
+			logger.Sugar().Errorw("UpdateGood", "DeviceInfoID", in.GetInfo().GetDeviceInfoID(), "error", err)
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, err.Error())
+		}
+	}
+
+	if in.GetInfo().DurationDays != nil && in.GetInfo().GetDurationDays() <= 0 {
+		logger.Sugar().Errorw("UpdateGood", "DurationDays", in.GetInfo().GetDurationDays())
+		return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, "DurationDays is invalid")
+	}
+
+	if in.GetInfo().CoinTypeID != nil {
+		if _, err := uuid.Parse(in.GetInfo().GetCoinTypeID()); err != nil {
+			logger.Sugar().Errorw("UpdateGood", "CoinTypeID", in.GetInfo().GetCoinTypeID(), "error", err)
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, err.Error())
+		}
+	}
+
+	if in.GetInfo().InheritFromGoodID != nil {
+		if _, err := uuid.Parse(in.GetInfo().GetInheritFromGoodID()); err != nil {
+			logger.Sugar().Errorw("UpdateGood", "InheritFromGoodID", in.GetInfo().GetInheritFromGoodID(), "error", err)
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, err.Error())
+		}
+	}
+
+	if in.GetInfo().VendorLocationID != nil {
+		if _, err := uuid.Parse(in.GetInfo().GetVendorLocationID()); err != nil {
+			logger.Sugar().Errorw("UpdateGood", "VendorLocationID", in.GetInfo().GetVendorLocationID(), "error", err)
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, err.Error())
+		}
+	}
+
+	if in.GetInfo().Price != nil {
+		if price, err := decimal.NewFromString(in.GetInfo().GetPrice()); err != nil || price.Cmp(decimal.NewFromInt(0)) <= 0 {
+			logger.Sugar().Errorw("UpdateGood", "Price", in.GetInfo().GetPrice(), "error", err)
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, "Price is invalid")
+		}
+	}
+
+	if in.GetInfo().BenefitType != nil {
+		switch in.GetInfo().GetBenefitType() {
+		case mgrpb.BenefitType_BenefitTypePlatform:
+		case mgrpb.BenefitType_BenefitTypePool:
+		default:
+			logger.Sugar().Errorw("UpdateGood", "BenefitType", in.GetInfo().GetBenefitType())
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, "BenefitType is invalid")
+		}
+	}
+
+	if in.GetInfo().GoodType != nil {
+		switch in.GetInfo().GetGoodType() {
+		case mgrpb.GoodType_GoodTypeClassicMining:
+		case mgrpb.GoodType_GoodTypeUnionMining:
+		case mgrpb.GoodType_GoodTypeTechniqueFee:
+		case mgrpb.GoodType_GoodTypeElectricityFee:
+		default:
+			logger.Sugar().Errorw("UpdateGood", "GoodType", in.GetInfo().GetGoodType())
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, "GoodType is invalid")
+		}
+	}
+
+	if in.GetInfo().Title != nil {
+		if in.GetInfo().GetTitle() == "" {
+			logger.Sugar().Errorw("UpdateGood", "Title", in.GetInfo().GetTitle())
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, "Title is invalid")
+		}
+	}
+
+	if in.GetInfo().UnitAmount != nil {
+		if in.GetInfo().GetUnitAmount() <= 0 {
+			logger.Sugar().Errorw("UpdateGood", "UnitAmount", in.GetInfo().GetUnitAmount())
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, "UnitAmount is invalid")
+		}
+	}
+
+	for _, coinTypeID := range in.GetInfo().GetSupportCoinTypeIDs() {
+		if _, err := uuid.Parse(coinTypeID); err != nil {
+			logger.Sugar().Errorw("UpdateGood", "SupportCoinTypeIDs", in.GetInfo().GetSupportCoinTypeIDs(), "error", err)
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, err.Error())
+		}
+	}
+
+	now := uint32(time.Now().Unix())
+
+	if in.GetInfo().DeliveryAt != nil {
+		if in.GetInfo().GetDeliveryAt() <= now {
+			logger.Sugar().Errorw("UpdateGood", "DeliveryAt", in.GetInfo().GetDeliveryAt(), "now", now)
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, "DeliveryAt is invalid")
+		}
+	}
+
+	if in.GetInfo().StartAt != nil {
+		if in.GetInfo().GetStartAt() <= now {
+			logger.Sugar().Errorw("UpdateGood", "StartAt", in.GetInfo().GetStartAt(), "now", now)
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, "StartAt is invalid")
+		}
+	}
+
+	if in.GetInfo().Total != nil {
+		if in.GetInfo().GetTotal() <= 0 {
+			logger.Sugar().Errorw("UpdateGood", "Total", in.GetInfo().GetTotal())
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, "Total is invalid")
+		}
+	}
+
+	if in.GetInfo().Sold != nil {
+		if in.GetInfo().GetSold() <= 0 {
+			logger.Sugar().Errorw("UpdateGood", "Sold", in.GetInfo().GetSold())
+			return &npool.UpdateGoodResponse{}, status.Error(codes.InvalidArgument, "Sold is invalid")
+		}
+	}
+
+	span = commontracer.TraceInvoker(span, "Good", "mw", "UpdateGood")
+
+	info, err := goodmw.UpdateGood(ctx, in.GetInfo())
+	if err != nil {
+		logger.Sugar().Errorw("UpdateGood", "Good", in.GetInfo())
+		return &npool.UpdateGoodResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.UpdateGoodResponse{
+		Info: info,
+	}, nil
 }
---
+++ pkg/good/update.go
@@ -0,0 +1,11 @@
+package good
+
+import (
+	"context"
+
+	npool "github.com/NpoolPlatform/message/npool/good/mw/v1/good"
+)
+
+func UpdateGood(ctx context.Context, in *npool.GoodReq) (*npool.Good, error) {
+	return nil, nil
+}
