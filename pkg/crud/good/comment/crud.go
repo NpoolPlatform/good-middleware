a467334566abcdcb86a9abea8479aa7c85fbdcf7
Zhao KK <kikakkz@hotmail.com>
2023-08-11 08:04:07 +0000

    Add comment crud

--- pkg/crud/good/comment/crud.go
+++ pkg/crud/good/comment/crud.go
@@ -1,21 +1,11 @@
 package comment
 
 import (
-	"context"
 	"fmt"
-	"time"
 
-	constant "github.com/NpoolPlatform/good-manager/pkg/message/const"
-	commontracer "github.com/NpoolPlatform/good-manager/pkg/tracer"
-	tracer "github.com/NpoolPlatform/good-manager/pkg/tracer/comment"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/codes"
-
-	"github.com/NpoolPlatform/good-manager/pkg/db"
 	"github.com/NpoolPlatform/good-manager/pkg/db/ent"
-	"github.com/NpoolPlatform/good-manager/pkg/db/ent/comment"
+	entcomment "github.com/NpoolPlatform/good-manager/pkg/db/ent/comment"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	npool "github.com/NpoolPlatform/message/npool/good/mgr/v1/comment"
 
 	"github.com/google/uuid"
 )
@@ -26,420 +16,104 @@ type Req struct {
 	UserID    *uuid.UUID
 	GoodID    *uuid.UUID
 	OrderID   *uuid.UUID
-	Content   *uuid.UUID
+	Content   *string
 	ReplyToID *uuid.UUID
 }
 
-func CreateSet(c *ent.CommentCreate, in *npool.CommentReq) (*ent.CommentCreate, error) {
-	if in.ID != nil {
-		c.SetID(uuid.MustParse(in.GetID()))
+func CreateSet(c *ent.CommentCreate, req *Req) *ent.CommentCreate {
+	if req.ID != nil {
+		c.SetID(*req.ID)
 	}
-	if in.AppID != nil {
-		c.SetAppID(uuid.MustParse(in.GetAppID()))
+	if req.AppID != nil {
+		c.SetAppID(*req.AppID)
 	}
-	if in.UserID != nil {
-		c.SetUserID(uuid.MustParse(in.GetUserID()))
+	if req.UserID != nil {
+		c.SetUserID(*req.UserID)
 	}
-	if in.GoodID != nil {
-		c.SetGoodID(uuid.MustParse(in.GetGoodID()))
+	if req.GoodID != nil {
+		c.SetGoodID(*req.GoodID)
 	}
-	if in.OrderID != nil {
-		c.SetOrderID(uuid.MustParse(in.GetOrderID()))
+	if req.OrderID != nil {
+		c.SetOrderID(*req.OrderID)
 	}
-	if in.Content != nil {
-		c.SetContent(in.GetContent())
+	if req.Content != nil {
+		c.SetContent(*req.Content)
 	}
-	if in.ReplyToID != nil {
-		c.SetReplyToID(uuid.MustParse(in.GetReplyToID()))
+	if req.ReplyToID != nil {
+		c.SetReplyToID(*req.ReplyToID)
 	}
-	return c, nil
+	return c
 }
 
-func Create(ctx context.Context, in *npool.CommentReq) (*ent.Comment, error) {
-	var info *ent.Comment
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Create")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(codes.Error, "db operation fail")
-			span.RecordError(err)
-		}
-	}()
-
-	span = tracer.Trace(span, in)
-
-	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		c := cli.Comment.Create()
-		stm, err := CreateSet(c, in)
-		if err != nil {
-			return err
-		}
-		info, err = stm.Save(_ctx)
-		return err
-	})
-	if err != nil {
-		return nil, err
+func UpdateSet(u *ent.CommentUpdateOne, req *Req) *ent.CommentUpdateOne {
+	if req.Content != nil {
+		u.SetContent(*req.Content)
 	}
-
-	return info, nil
+	if req.ReplyToID != nil {
+		u.SetReplyToID(*req.ReplyToID)
+	}
+	return u
 }
 
-func CreateBulk(ctx context.Context, in []*npool.CommentReq) ([]*ent.Comment, error) {
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "CreateBulk")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(codes.Error, "db operation fail")
-			span.RecordError(err)
-		}
-	}()
-
-	span = tracer.TraceMany(span, in)
-
-	rows := []*ent.Comment{}
-	err = db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
-		bulk := make([]*ent.CommentCreate, len(in))
-		for i, info := range in {
-			bulk[i] = tx.Comment.Create()
-			bulk[i], err = CreateSet(bulk[i], info)
-			if err != nil {
-				return err
-			}
-		}
-		rows, err = tx.Comment.CreateBulk(bulk...).Save(_ctx)
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return rows, nil
+type Conds struct {
+	ID     *cruder.Cond
+	AppID  *cruder.Cond
+	UserID *cruder.Cond
+	GoodID *cruder.Cond
 }
 
-func UpdateSet(u *ent.CommentUpdateOne, in *npool.CommentReq) (*ent.CommentUpdateOne, error) {
-	if in.Content != nil {
-		u.SetContent(in.GetContent())
-	}
-	if in.ReplyToID != nil {
-		u.SetReplyToID(uuid.MustParse(in.GetReplyToID()))
-	}
-	return u, nil
-}
-
-func Update(ctx context.Context, in *npool.CommentReq) (*ent.Comment, error) {
-	var info *ent.Comment
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Update")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(codes.Error, "db operation fail")
-			span.RecordError(err)
-		}
-	}()
-
-	span = tracer.Trace(span, in)
-
-	err = db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
-		info, err = tx.Comment.Query().Where(comment.ID(uuid.MustParse(in.GetID()))).ForUpdate().Only(_ctx)
-		if err != nil {
-			return err
-		}
-
-		stm, err := UpdateSet(info.Update(), in)
-		if err != nil {
-			return err
-		}
-
-		info, err = stm.Save(_ctx)
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return info, nil
-}
-
-func Row(ctx context.Context, id uuid.UUID) (*ent.Comment, error) {
-	var info *ent.Comment
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Row")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(codes.Error, "db operation fail")
-			span.RecordError(err)
-		}
-	}()
-
-	span = commontracer.TraceID(span, id.String())
-
-	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		info, err = cli.Comment.Query().Where(comment.ID(id)).Only(_ctx)
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return info, nil
-}
-
-func setQueryConds(conds *npool.Conds, cli *ent.Client) (*ent.CommentQuery, error) {
-	stm := cli.Comment.Query()
+func SetQueryConds(q *ent.CommentQuery, conds *Conds) (*ent.CommentQuery, error) {
+	q.Where(entcomment.DeletedAt(0))
 	if conds == nil {
-		return stm, nil
+		return q, nil
 	}
 	if conds.ID != nil {
-		switch conds.GetID().GetOp() {
+		id, ok := conds.ID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid id")
+		}
+		switch conds.ID.Op {
 		case cruder.EQ:
-			stm.Where(comment.ID(uuid.MustParse(conds.GetID().GetValue())))
+			q.Where(entcomment.ID(id))
 		default:
 			return nil, fmt.Errorf("invalid comment field")
 		}
 	}
 	if conds.AppID != nil {
-		switch conds.GetAppID().GetOp() {
+		id, ok := conds.AppID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid appid")
+		}
+		switch conds.AppID.Op {
 		case cruder.EQ:
-			stm.Where(comment.AppID(uuid.MustParse(conds.GetAppID().GetValue())))
+			q.Where(entcomment.AppID(id))
 		default:
 			return nil, fmt.Errorf("invalid comment field")
 		}
 	}
 	if conds.UserID != nil {
-		switch conds.GetUserID().GetOp() {
+		id, ok := conds.UserID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid userid")
+		}
+		switch conds.UserID.Op {
 		case cruder.EQ:
-			stm.Where(comment.UserID(uuid.MustParse(conds.GetUserID().GetValue())))
+			q.Where(entcomment.UserID(id))
 		default:
 			return nil, fmt.Errorf("invalid comment field")
 		}
 	}
 	if conds.GoodID != nil {
-		switch conds.GetGoodID().GetOp() {
+		id, ok := conds.GoodID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid goodid")
+		}
+		switch conds.GoodID.Op {
 		case cruder.EQ:
-			stm.Where(comment.GoodID(uuid.MustParse(conds.GetGoodID().GetValue())))
+			q.Where(entcomment.GoodID(id))
 		default:
 			return nil, fmt.Errorf("invalid comment field")
 		}
 	}
-	return stm, nil
-}
-
-func Rows(ctx context.Context, conds *npool.Conds, offset, limit int) ([]*ent.Comment, int, error) {
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Rows")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(codes.Error, "db operation fail")
-			span.RecordError(err)
-		}
-	}()
-
-	span = tracer.TraceConds(span, conds)
-	span = commontracer.TraceOffsetLimit(span, offset, limit)
-
-	rows := []*ent.Comment{}
-	var total int
-	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		stm, err := setQueryConds(conds, cli)
-		if err != nil {
-			return err
-		}
-
-		total, err = stm.Count(_ctx)
-		if err != nil {
-			return err
-		}
-
-		rows, err = stm.
-			Offset(offset).
-			Order(ent.Desc(comment.FieldUpdatedAt)).
-			Limit(limit).
-			All(_ctx)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil, 0, err
-	}
-	return rows, total, nil
-}
-
-func RowOnly(ctx context.Context, conds *npool.Conds) (*ent.Comment, error) {
-	var info *ent.Comment
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "RowOnly")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(codes.Error, "db operation fail")
-			span.RecordError(err)
-		}
-	}()
-
-	span = tracer.TraceConds(span, conds)
-
-	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		stm, err := setQueryConds(conds, cli)
-		if err != nil {
-			return err
-		}
-
-		info, err = stm.Only(_ctx)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return info, nil
-}
-
-func Count(ctx context.Context, conds *npool.Conds) (uint32, error) {
-	var err error
-	var total int
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Count")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(codes.Error, "db operation fail")
-			span.RecordError(err)
-		}
-	}()
-
-	span = tracer.TraceConds(span, conds)
-
-	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		stm, err := setQueryConds(conds, cli)
-		if err != nil {
-			return err
-		}
-
-		total, err = stm.Count(_ctx)
-		if err != nil {
-			return err
-		}
-		return nil
-	})
-	if err != nil {
-		return 0, err
-	}
-
-	return uint32(total), nil
-}
-
-func Exist(ctx context.Context, id uuid.UUID) (bool, error) {
-	var err error
-	exist := false
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Exist")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(codes.Error, "db operation fail")
-			span.RecordError(err)
-		}
-	}()
-
-	span = commontracer.TraceID(span, id.String())
-
-	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		exist, err = cli.Comment.Query().Where(comment.ID(id)).Exist(_ctx)
-		return err
-	})
-	if err != nil {
-		return false, err
-	}
-
-	return exist, nil
-}
-
-func ExistConds(ctx context.Context, conds *npool.Conds) (bool, error) {
-	var err error
-	exist := false
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "ExistConds")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(codes.Error, "db operation fail")
-			span.RecordError(err)
-		}
-	}()
-
-	span = tracer.TraceConds(span, conds)
-
-	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		stm, err := setQueryConds(conds, cli)
-		if err != nil {
-			return err
-		}
-
-		exist, err = stm.Exist(_ctx)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
-	if err != nil {
-		return false, err
-	}
-
-	return exist, nil
-}
-
-func Delete(ctx context.Context, id string) (*ent.Comment, error) {
-	var info *ent.Comment
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Delete")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(codes.Error, "db operation fail")
-			span.RecordError(err)
-		}
-	}()
-
-	span = commontracer.TraceID(span, id)
-
-	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		info, err = cli.Comment.UpdateOneID(uuid.MustParse(id)).
-			SetDeletedAt(uint32(time.Now().Unix())).
-			Save(_ctx)
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return info, nil
+	return q, nil
 }
