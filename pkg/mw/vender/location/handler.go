9c46b0daa25aeb2231ccd2075e99d0d3ff771f77
Zhao KK <kikakkz@hotmail.com>
2023-08-11 13:28:48 +0000

    Add vendor location and vendor brand

--- pkg/mw/vender/location/handler.go
+++ pkg/mw/vender/location/handler.go
@@ -1,20 +1,20 @@
-package vendorlocation
+package location
 
 import (
 	"context"
 	"fmt"
 
 	constant "github.com/NpoolPlatform/good-middleware/pkg/const"
-	vendorlocationcrud "github.com/NpoolPlatform/good-middleware/pkg/crud/vendorlocation"
+	locationcrud "github.com/NpoolPlatform/good-middleware/pkg/crud/vender/location"
 	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	npool "github.com/NpoolPlatform/message/npool/good/mw/v1/vendorlocation"
+	npool "github.com/NpoolPlatform/message/npool/good/mw/v1/vender/location"
 
 	"github.com/google/uuid"
 )
 
 type Handler struct {
-	vendorlocationcrud.Req
-	Conds  *vendorlocationcrud.Conds
+	locationcrud.Req
+	Conds  *locationcrud.Conds
 	Offset int32
 	Limit  int32
 }
@@ -43,63 +43,79 @@ func WithID(id *string) func(context.Context, *Handler) error {
 	}
 }
 
-func WithType(s *string) func(context.Context, *Handler) error {
+func WithCountry(s *string) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
 		if s == nil {
 			return nil
 		}
-		const leastTypeLen = 3
-		if len(*s) <= leastTypeLen {
-			return fmt.Errorf("invalid type")
+		const leastCountryLen = 3
+		if len(*s) <= leastCountryLen {
+			return fmt.Errorf("invalid country")
 		}
-		h.Type = s
+		h.Country = s
 		return nil
 	}
 }
 
-func WithManufacturer(s *string) func(context.Context, *Handler) error {
+func WithProvince(s *string) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
 		if s == nil {
 			return nil
 		}
-		const leastManufacturerLen = 3
-		if len(*s) <= leastManufacturerLen {
-			return fmt.Errorf("invalid manufacturer")
+		const leastProvinceLen = 3
+		if len(*s) <= leastProvinceLen {
+			return fmt.Errorf("invalid province")
 		}
-		h.Manufacturer = s
+		h.Province = s
 		return nil
 	}
 }
 
-func WithPowerComsuption(s *uint32) func(context.Context, *Handler) error {
+func WithCity(s *string) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
-		h.PowerComsuption = s
-		return nil
-	}
-}
-
-func WithShipmentAt(n *uint32) func(context.Context, *Handler) error {
-	return func(ctx context.Context, h *Handler) error {
-		h.ShipmentAt = n
-		return nil
-	}
-}
-
-func WithPosters(ss []string) func(context.Context, *Handler) error {
-	return func(ctx context.Context, h *Handler) error {
-		for _, s := range ss {
-			if s == "" {
-				return fmt.Errorf("invalid poster")
-			}
+		if s == nil {
+			return nil
 		}
-		h.Posters = ss
+		const leastCityLen = 3
+		if len(*s) <= leastCityLen {
+			return fmt.Errorf("invalid city")
+		}
+		h.City = s
+		return nil
+	}
+}
+
+func WithAddress(s *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if s == nil {
+			return nil
+		}
+		const leastAddressLen = 3
+		if len(*s) <= leastAddressLen {
+			return fmt.Errorf("invalid address")
+		}
+		h.Address = s
+		return nil
+	}
+}
+
+func WithBrandID(id *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.BrandID = &_id
 		return nil
 	}
 }
 
 func WithConds(conds *npool.Conds) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
-		h.Conds = &vendorlocationcrud.Conds{}
+		h.Conds = &locationcrud.Conds{}
 		if conds == nil {
 			return nil
 		}
@@ -110,16 +126,26 @@ func WithConds(conds *npool.Conds) func(context.Context, *Handler) error {
 			}
 			h.Conds.ID = &cruder.Cond{Op: conds.GetID().GetOp(), Val: id}
 		}
-		if conds.Type != nil {
-			h.Conds.Type = &cruder.Cond{
-				Op:  conds.GetType().GetOp(),
-				Val: conds.GetType().GetValue(),
+		if conds.Country != nil {
+			h.Conds.Country = &cruder.Cond{
+				Op:  conds.GetCountry().GetOp(),
+				Val: conds.GetCountry().GetValue(),
 			}
 		}
-		if conds.Manufacturer != nil {
-			h.Conds.Manufacturer = &cruder.Cond{
-				Op:  conds.GetManufacturer().GetOp(),
-				Val: conds.GetManufacturer().GetValue(),
+		if conds.Province != nil {
+			h.Conds.Province = &cruder.Cond{
+				Op:  conds.GetProvince().GetOp(),
+				Val: conds.GetProvince().GetValue(),
+			}
+		}
+		if conds.BrandID != nil {
+			id, err := uuid.Parse(conds.GetBrandID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.BrandID = &cruder.Cond{
+				Op:  conds.GetBrandID().GetOp(),
+				Val: id,
 			}
 		}
 		return nil
