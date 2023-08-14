7c0f9141ecb7a39f73d27cf3fdc7a70e627ce0fa
Zhao KK <kikakkz@hotmail.com>
2023-08-14 10:01:29 +0000

    Add basic unit test

--- pkg/mw/good/good_test.go
+++ pkg/mw/good/good_test.go
@@ -8,9 +8,15 @@ import (
 	"testing"
 	"time"
 
+	types "github.com/NpoolPlatform/message/npool/basetypes/good/v1"
 	npool "github.com/NpoolPlatform/message/npool/good/mw/v1/good"
 
+	deviceinfo1 "github.com/NpoolPlatform/good-middleware/pkg/mw/deviceinfo"
+	vendorbrand1 "github.com/NpoolPlatform/good-middleware/pkg/mw/vender/brand"
+	vendorlocation1 "github.com/NpoolPlatform/good-middleware/pkg/mw/vender/location"
+
 	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/NpoolPlatform/good-middleware/pkg/testinit"
@@ -26,27 +32,89 @@ func init() {
 }
 
 var ret = npool.Good{
-	ID:                   uuid.NewString(),
-	DeviceInfoID:         uuid.NewString(),
-	CoinTypeID:           uuid.NewString(),
-	VendorLocationID:     uuid.NewString(),
-	GoodType:             types.GoodType_PowerRenting,
-	BenefitType:          types.BenefitType_BenefitTypePlatform,
-	Price:                decimal.NewFromInt(123).String(),
-	Title:                uuid.NewString(),
-	Unit:                 "TiB",
-	UnitAmount:           1,
-	SupportCoinTypeIDs:   []string{uuid.NewString(), uuid.NewString()},
-	TestOnly:             true,
-	Posters:              []string{uuid.NewString(), uuid.NewString()},
-	Labels:               []string{uuid.NewString(), uuid.NewString()},
-	GoodTotal:            decimal.NewFromInt(1000).String(),
-	DeliveryAt:           uint32(time.Now().Unix() + 1000),
-	StartAt:              uint32(time.Now().Unix() + 1000),
-	BenefitIntervalHours: 24,
-	GoodAppLocked:        decimal.NewFromInt(0).String(),
+	ID:                     uuid.NewString(),
+	DeviceInfoID:           uuid.NewString(),
+	DeviceType:             uuid.NewString(),
+	DeviceManufacturer:     uuid.NewString(),
+	DevicePowerComsuption:  120,
+	DeviceShipmentAt:       uint32(time.Now().Unix() - 1000),
+	DevicePosters:          []string{uuid.NewString(), uuid.NewString()},
+	DurationDays:           14,
+	CoinTypeID:             uuid.NewString(),
+	VendorLocationID:       uuid.NewString(),
+	VendorLocationCountry:  uuid.NewString(),
+	VendorLocationProvince: uuid.NewString(),
+	VendorLocationCity:     uuid.NewString(),
+	VendorLocationAddress:  uuid.NewString(),
+	VendorBrandName:        uuid.NewString(),
+	VendorBrandLogo:        uuid.NewString(),
+	GoodType:               types.GoodType_PowerRenting,
+	BenefitType:            types.BenefitType_BenefitTypePlatform,
+	Price:                  decimal.NewFromInt(123).String(),
+	Title:                  uuid.NewString(),
+	Unit:                   "TiB",
+	UnitAmount:             1,
+	SupportCoinTypeIDs:     []string{uuid.NewString(), uuid.NewString()},
+	TestOnly:               true,
+	Posters:                []string{uuid.NewString(), uuid.NewString()},
+	Labels:                 []string{uuid.NewString(), uuid.NewString()},
+	GoodTotal:              decimal.NewFromInt(1000).String(),
+	DeliveryAt:             uint32(time.Now().Unix() + 1000),
+	StartAt:                uint32(time.Now().Unix() + 1000),
+	BenefitIntervalHours:   24,
+	GoodAppLocked:          decimal.NewFromInt(0).String(),
 }
 
 func setup(t *testing.T) func(*testing.T) {
+	h1, err := vendorbrand1.NewHandler(
+		context.Background(),
+		vendorbrand1.WithName(&ret.VendorBrandName),
+		vendorbrand1.WithLogo(&ret.VendorBrandLogo),
+	)
+	assert.Nil(t, err)
 
+	info1, err := h1.CreateBrand(context.Background())
+	assert.Nil(t, err)
+
+	h2, err := vendorlocation1.NewHandler(
+		context.Background(),
+		vendorlocation1.WithID(&ret.VendorLocationID),
+		vendorlocation1.WithCountry(&ret.VendorLocationCountry),
+		vendorlocation1.WithProvince(&ret.VendorLocationProvince),
+		vendorlocation1.WithCity(&ret.VendorLocationCity),
+		vendorlocation1.WithAddress(&ret.VendorLocationAddress),
+		vendorlocation1.WithBrandID(&info1.ID),
+	)
+	assert.Nil(t, err)
+
+	_, err = h2.CreateLocation(context.Background())
+	assert.Nil(t, err)
+
+	h3, err := deviceinfo1.NewHandler(
+		context.Background(),
+		deviceinfo1.WithID(&ret.DeviceInfoID),
+		deviceinfo1.WithType(&ret.DeviceType),
+		deviceinfo1.WithManufacturer(&ret.DeviceManufacturer),
+		deviceinfo1.WithPowerComsuption(&ret.DevicePowerComsuption),
+		deviceinfo1.WithShipmentAt(&ret.DeviceShipmentAt),
+		deviceinfo1.WithPosters(ret.DevicePosters),
+	)
+	assert.Nil(t, err)
+
+	_, err = h3.CreateDeviceInfo(context.Background())
+
+	return func(*testing.T) {
+		_, _ = h3.DeleteDeviceInfo(context.Background())
+		_, _ = h2.DeleteLocation(context.Background())
+		_, _ = h1.DeleteBrand(context.Background())
+	}
+}
+
+func TestGood(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+
+	teardown := setup(t)
+	defer teardown(t)
 }
