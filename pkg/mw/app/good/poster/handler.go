6cb41690edb9231483793f413db9c510a7eb7f17
Zhao KK <kikakkz@hotmail.com>
2024-03-21 11:34:46 +0000

    Add app good poster

---
+++ pkg/mw/app/good/poster/handler.go
@@ -0,0 +1,207 @@
+package poster
+
+import (
+	"context"
+	"fmt"
+
+	constant "github.com/NpoolPlatform/good-middleware/pkg/const"
+	appgoodpostercrud "github.com/NpoolPlatform/good-middleware/pkg/crud/app/good/poster"
+	goodbasecrud "github.com/NpoolPlatform/good-middleware/pkg/crud/good/goodbase"
+	appgoodbase1 "github.com/NpoolPlatform/good-middleware/pkg/mw/app/good/goodbase"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	npool "github.com/NpoolPlatform/message/npool/good/mw/v1/app/good/poster"
+
+	"github.com/google/uuid"
+)
+
+type Handler struct {
+	ID *uint32
+	appgoodpostercrud.Req
+	PosterConds   *appgoodpostercrud.Conds
+	GoodBaseConds *goodbasecrud.Conds
+	Offset        int32
+	Limit         int32
+}
+
+func NewHandler(ctx context.Context, options ...func(context.Context, *Handler) error) (*Handler, error) {
+	handler := &Handler{
+		PosterConds:   &appgoodpostercrud.Conds{},
+		GoodBaseConds: &goodbasecrud.Conds{},
+	}
+	for _, opt := range options {
+		if err := opt(ctx, handler); err != nil {
+			return nil, err
+		}
+	}
+	return handler, nil
+}
+
+func WithID(id *uint32, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid id")
+			}
+			return nil
+		}
+		h.ID = id
+		return nil
+	}
+}
+
+func WithEntID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid entid")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.EntID = &_id
+		return nil
+	}
+}
+
+func WithAppGoodID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		handler, err := appgoodbase1.NewHandler(
+			ctx,
+			appgoodbase1.WithEntID(id, true),
+		)
+		if err != nil {
+			return err
+		}
+		exist, err := handler.ExistGoodBase(ctx)
+		if err != nil {
+			return err
+		}
+		if !exist {
+			return fmt.Errorf("invalid appgood")
+		}
+		h.AppGoodID = handler.EntID
+		return nil
+	}
+}
+
+func WithPoster(s *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Poster = s
+		return nil
+	}
+}
+
+func WithIndex(n *uint8, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Index = n
+		return nil
+	}
+}
+
+func (h *Handler) withAppPosterGoodConds(conds *npool.Conds) error {
+	if conds.ID != nil {
+		h.PosterConds.ID = &cruder.Cond{
+			Op:  conds.GetID().GetOp(),
+			Val: conds.GetID().GetValue(),
+		}
+	}
+	if conds.EntID != nil {
+		id, err := uuid.Parse(conds.GetEntID().GetValue())
+		if err != nil {
+			return err
+		}
+		h.PosterConds.EntID = &cruder.Cond{
+			Op:  conds.GetEntID().GetOp(),
+			Val: id,
+		}
+	}
+	if conds.AppGoodID != nil {
+		id, err := uuid.Parse(conds.GetAppGoodID().GetValue())
+		if err != nil {
+			return err
+		}
+		h.PosterConds.AppGoodID = &cruder.Cond{
+			Op:  conds.GetAppGoodID().GetOp(),
+			Val: id,
+		}
+	}
+	if conds.AppGoodIDs != nil {
+		ids := []uuid.UUID{}
+		for _, id := range conds.GetAppGoodIDs().GetValue() {
+			_id, err := uuid.Parse(id)
+			if err != nil {
+				return err
+			}
+			ids = append(ids, _id)
+		}
+		h.PosterConds.AppGoodIDs = &cruder.Cond{
+			Op:  conds.GetAppGoodIDs().GetOp(),
+			Val: ids,
+		}
+	}
+	return nil
+}
+
+func (h *Handler) withGoodBaseConds(conds *npool.Conds) error {
+	if conds.GoodID != nil {
+		id, err := uuid.Parse(conds.GetGoodID().GetValue())
+		if err != nil {
+			return err
+		}
+		h.GoodBaseConds.EntID = &cruder.Cond{
+			Op:  conds.GetGoodID().GetOp(),
+			Val: id,
+		}
+	}
+	if conds.GoodIDs != nil {
+		ids := []uuid.UUID{}
+		for _, id := range conds.GetGoodIDs().GetValue() {
+			_id, err := uuid.Parse(id)
+			if err != nil {
+				return err
+			}
+			ids = append(ids, _id)
+		}
+		h.GoodBaseConds.EntIDs = &cruder.Cond{
+			Op:  conds.GetGoodIDs().GetOp(),
+			Val: ids,
+		}
+	}
+	return nil
+}
+
+//nolint:gocyclo
+func WithConds(conds *npool.Conds) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if conds == nil {
+			return nil
+		}
+		if err := h.withAppPosterGoodConds(conds); err != nil {
+			return err
+		}
+		if err := h.withGoodBaseConds(conds); err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func WithOffset(value int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Offset = value
+		return nil
+	}
+}
+
+func WithLimit(value int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if value == 0 {
+			value = constant.DefaultRowLimit
+		}
+		h.Limit = value
+		return nil
+	}
+}
