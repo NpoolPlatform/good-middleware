8ba23ac0d396420188769ca5ebb9012a33fba5d0
Zhao KK <kikakkz@hotmail.com>
2024-04-07 11:34:52 +0000

    Add recommend client unit test

--- pkg/client/app/good/recommend/client.go
+++ pkg/client/app/good/recommend/client.go
@@ -6,53 +6,34 @@ import (
 	"time"
 
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
-
-	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	npool "github.com/NpoolPlatform/message/npool/good/mw/v1/app/good/recommend"
-
 	servicename "github.com/NpoolPlatform/good-middleware/pkg/servicename"
+	npool "github.com/NpoolPlatform/message/npool/good/mw/v1/app/good/recommend"
+	"google.golang.org/grpc"
 )
 
-var timeout = 10 * time.Second
-
-type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
-
-func do(ctx context.Context, handler handler) (cruder.Any, error) {
-	_ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-
-	conn, err := grpc2.GetGRPCConn(servicename.ServiceDomain, grpc2.GRPCTAG)
-	if err != nil {
-		return nil, err
-	}
-
-	defer conn.Close()
-
-	cli := npool.NewMiddlewareClient(conn)
-
-	return handler(_ctx, cli)
+func withClient(ctx context.Context, handler func(context.Context, npool.MiddlewareClient) (interface{}, error)) (interface{}, error) {
+	return grpc2.WithGRPCConn(
+		ctx,
+		servicename.ServiceDomain,
+		10*time.Second,
+		func(_ctx context.Context, conn *grpc.ClientConn) (interface{}, error) {
+			return handler(_ctx, npool.NewMiddlewareClient(conn))
+		},
+		grpc2.GRPCTAG,
+	)
 }
 
-func CreateRecommend(ctx context.Context, in *npool.RecommendReq) (*npool.Recommend, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.CreateRecommend(ctx, &npool.CreateRecommendRequest{
-			Info: in,
+func CreateRecommend(ctx context.Context, req *npool.RecommendReq) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.CreateRecommend(_ctx, &npool.CreateRecommendRequest{
+			Info: req,
 		})
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
 	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.Recommend), nil
+	return err
 }
 
-func GetRecommends(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.Recommend, uint32, error) {
-	total := uint32(0)
-
-	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+func GetRecommends(ctx context.Context, conds *npool.Conds, offset, limit int32) (intos []*npool.Recommend, total uint32, err error) {
+	_infos, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
 		resp, err := cli.GetRecommends(ctx, &npool.GetRecommendsRequest{
 			Conds:  conds,
 			Offset: offset,
@@ -61,24 +42,21 @@ func GetRecommends(ctx context.Context, conds *npool.Conds, offset, limit int32)
 		if err != nil {
 			return nil, err
 		}
-
 		total = resp.Total
-
 		return resp.Infos, nil
 	})
 	if err != nil {
 		return nil, 0, err
 	}
-	return infos.([]*npool.Recommend), total, nil
+	return _infos.([]*npool.Recommend), total, nil
 }
 
 func GetRecommendOnly(ctx context.Context, conds *npool.Conds) (*npool.Recommend, error) {
-	const limit = 2
-	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+	infos, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
 		resp, err := cli.GetRecommends(ctx, &npool.GetRecommendsRequest{
 			Conds:  conds,
 			Offset: 0,
-			Limit:  limit,
+			Limit:  2,
 		})
 		if err != nil {
 			return nil, err
@@ -98,7 +76,7 @@ func GetRecommendOnly(ctx context.Context, conds *npool.Conds) (*npool.Recommend
 }
 
 func ExistRecommendConds(ctx context.Context, conds *npool.Conds) (bool, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+	info, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
 		resp, err := cli.ExistRecommendConds(ctx, &npool.ExistRecommendCondsRequest{
 			Conds: conds,
 		})
@@ -113,42 +91,29 @@ func ExistRecommendConds(ctx context.Context, conds *npool.Conds) (bool, error) 
 	return info.(bool), nil
 }
 
-func UpdateRecommend(ctx context.Context, in *npool.RecommendReq) (*npool.Recommend, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.UpdateRecommend(ctx, &npool.UpdateRecommendRequest{
-			Info: in,
+func UpdateRecommend(ctx context.Context, req *npool.RecommendReq) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.UpdateRecommend(_ctx, &npool.UpdateRecommendRequest{
+			Info: req,
 		})
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
 	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.Recommend), nil
+	return err
 }
 
-func DeleteRecommend(ctx context.Context, id uint32) (*npool.Recommend, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.DeleteRecommend(ctx, &npool.DeleteRecommendRequest{
+func DeleteRecommend(ctx context.Context, id *uint32, entID *string) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.DeleteRecommend(_ctx, &npool.DeleteRecommendRequest{
 			Info: &npool.RecommendReq{
-				ID: &id,
+				ID:    id,
+				EntID: entID,
 			},
 		})
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
 	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.Recommend), nil
+	return err
 }
 
 func GetRecommend(ctx context.Context, id string) (*npool.Recommend, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+	info, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
 		resp, err := cli.GetRecommend(ctx, &npool.GetRecommendRequest{
 			EntID: id,
 		})
--- pkg/client/app/good/recommend/client_test.go
+++ pkg/client/app/good/recommend/client_test.go
@@ -1,4 +1,3 @@
-//nolint:dupl
 package recommend
 
 import (
@@ -15,16 +14,22 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
+	apppowerrental1 "github.com/NpoolPlatform/good-middleware/pkg/client/app/powerrental"
 	devicetype1 "github.com/NpoolPlatform/good-middleware/pkg/client/device"
-	vendorbrand1 "github.com/NpoolPlatform/good-middleware/pkg/client/vender/brand"
-	vendorlocation1 "github.com/NpoolPlatform/good-middleware/pkg/client/vender/location"
+	manufacturer1 "github.com/NpoolPlatform/good-middleware/pkg/client/device/manufacturer"
+	powerrental1 "github.com/NpoolPlatform/good-middleware/pkg/client/powerrental"
+	brand1 "github.com/NpoolPlatform/good-middleware/pkg/client/vender/brand"
+	location1 "github.com/NpoolPlatform/good-middleware/pkg/client/vender/location"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	types "github.com/NpoolPlatform/message/npool/basetypes/good/v1"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 	npool "github.com/NpoolPlatform/message/npool/good/mw/v1/app/good/recommend"
+	apppowerrentalmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/app/powerrental"
 	devicetypemwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/device"
-	vendorbrandmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/vender/brand"
-	vendorlocationmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/vender/location"
+	manufacturermwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/device/manufacturer"
+	powerrentalmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/powerrental"
+	brandmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/vender/brand"
+	locationmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/vender/location"
 
 	"github.com/google/uuid"
 	"github.com/shopspring/decimal"
@@ -42,151 +47,138 @@ func init() {
 	}
 }
 
-var good = goodmwpb.Good{
-	EntID:                  uuid.NewString(),
-	DeviceInfoID:           uuid.NewString(),
-	DeviceType:             uuid.NewString(),
-	DeviceManufacturer:     uuid.NewString(),
-	DevicePowerConsumption: 120,
-	DeviceShipmentAt:       uint32(time.Now().Unix() - 1000),
-	DevicePosters:          []string{uuid.NewString(), uuid.NewString()},
-	CoinTypeID:             uuid.NewString(),
-	VendorLocationID:       uuid.NewString(),
-	VendorLocationCountry:  uuid.NewString(),
-	VendorLocationProvince: uuid.NewString(),
-	VendorLocationCity:     uuid.NewString(),
-	VendorLocationAddress:  uuid.NewString(),
-	VendorBrandName:        uuid.NewString(),
-	VendorBrandLogo:        uuid.NewString(),
-	GoodType:               types.GoodType_PowerRental,
-	BenefitType:            types.BenefitType_BenefitTypePlatform,
-	UnitPrice:              decimal.NewFromInt(123).String(),
-	Title:                  uuid.NewString(),
-	QuantityUnit:           "TiB",
-	QuantityUnitAmount:     "1",
-	TestOnly:               true,
-	Posters:                []string{uuid.NewString(), uuid.NewString()},
-	Labels: []types.GoodLabel{
-		types.GoodLabel_GoodLabelInnovationStarter,
-		types.GoodLabel_GoodLabelNoviceExclusive,
-	},
-	GoodTotal:            decimal.NewFromInt(1000).String(),
-	GoodLocked:           decimal.NewFromInt(0).String(),
-	GoodInService:        decimal.NewFromInt(0).String(),
-	GoodWaitStart:        decimal.NewFromInt(0).String(),
-	GoodSold:             decimal.NewFromInt(0).String(),
-	DeliveryAt:           uint32(time.Now().Unix() + 1000),
-	StartAt:              uint32(time.Now().Unix() + 1000),
-	BenefitIntervalHours: 24,
-	GoodAppReserved:      decimal.NewFromInt(0).String(),
-	UnitLockDeposit:      decimal.NewFromInt(1).String(),
-}
-
 var ret = npool.Recommend{
 	EntID:          uuid.NewString(),
 	AppID:          uuid.NewString(),
 	RecommenderID:  uuid.NewString(),
-	GoodID:         good.EntID,
-	RecommendIndex: decimal.RequireFromString("4.9").String(),
+	AppGoodID:      uuid.NewString(),
+	GoodName:       uuid.NewString(),
+	RecommendIndex: decimal.NewFromInt(4).String(),
 	Message:        uuid.NewString(),
-	GoodName:       good.Title,
 }
 
 func setup(t *testing.T) func(*testing.T) {
-	info1, err := vendorbrand1.CreateBrand(context.Background(), &vendorbrandmwpb.BrandReq{
-		Name: &good.VendorBrandName,
-		Logo: &good.VendorBrandLogo,
-	})
-	assert.Nil(t, err)
-	assert.NotNil(t, info1)
-
-	vendorLocation, err := vendorlocation1.CreateLocation(context.Background(), &vendorlocationmwpb.LocationReq{
-		EntID:    &good.VendorLocationID,
-		Country:  &good.VendorLocationCountry,
-		Province: &good.VendorLocationProvince,
-		City:     &good.VendorLocationCity,
-		Address:  &good.VendorLocationAddress,
-		BrandID:  &info1.EntID,
+	manufacturerID := uuid.NewString()
+	err := manufacturer1.CreateManufacturer(context.Background(), &manufacturermwpb.ManufacturerReq{
+		EntID: &manufacturerID,
+		Name:  func() *string { s := uuid.NewString(); return &s }(),
+		Logo:  func() *string { s := uuid.NewString(); return &s }(),
 	})
 	assert.Nil(t, err)
 
-	device, err := devicetype1.CreateDeviceInfo(context.Background(), &devicetypemwpb.DeviceInfoReq{
-		EntID:            &good.DeviceInfoID,
-		Type:             &good.DeviceType,
-		Manufacturer:     &good.DeviceManufacturer,
-		PowerConsumption: &good.DevicePowerConsumption,
-		ShipmentAt:       &good.DeviceShipmentAt,
-		Posters:          good.DevicePosters,
+	deviceTypeID := uuid.NewString()
+	err = devicetype1.CreateDeviceType(context.Background(), &devicetypemwpb.DeviceTypeReq{
+		EntID:            &deviceTypeID,
+		Type:             func() *string { s := uuid.NewString(); return &s }(),
+		ManufacturerID:   &manufacturerID,
+		PowerConsumption: func() *uint32 { u := uint32(120); return &u }(),
+		ShipmentAt:       func() *uint32 { u := uint32(time.Now().Unix()); return &u }(),
 	})
 	assert.Nil(t, err)
 
-	_, err = good1.CreateGood(context.Background(), &goodmwpb.GoodReq{
-		EntID:                &good.EntID,
-		DeviceInfoID:         &good.DeviceInfoID,
-		CoinTypeID:           &good.CoinTypeID,
-		VendorLocationID:     &good.VendorLocationID,
-		UnitPrice:            &good.UnitPrice,
-		BenefitType:          &good.BenefitType,
-		GoodType:             &good.GoodType,
-		Title:                &good.Title,
-		QuantityUnit:         &good.QuantityUnit,
-		QuantityUnitAmount:   &good.QuantityUnitAmount,
-		DeliveryAt:           &good.DeliveryAt,
-		StartAt:              &good.StartAt,
-		TestOnly:             &good.TestOnly,
-		BenefitIntervalHours: &good.BenefitIntervalHours,
-		UnitLockDeposit:      &good.UnitLockDeposit,
-		Total:                &good.GoodTotal,
-		Posters:              good.Posters,
-		Labels:               good.Labels,
+	brandID := uuid.NewString()
+	err = brand1.CreateBrand(context.Background(), &brandmwpb.BrandReq{
+		EntID: &brandID,
+		Name:  func() *string { s := uuid.NewString(); return &s }(),
+		Logo:  func() *string { s := uuid.NewString(); return &s }(),
+	})
+	assert.Nil(t, err)
+
+	locationID := uuid.NewString()
+	err = location1.CreateLocation(context.Background(), &locationmwpb.LocationReq{
+		EntID:    &locationID,
+		Country:  func() *string { s := uuid.NewString(); return &s }(),
+		Province: func() *string { s := uuid.NewString(); return &s }(),
+		City:     func() *string { s := uuid.NewString(); return &s }(),
+		Address:  func() *string { s := uuid.NewString(); return &s }(),
+		BrandID:  &brandID,
+	})
+	assert.Nil(t, err)
+
+	goodID := uuid.NewString()
+	err = powerrental1.CreatePowerRental(context.Background(), &powerrentalmwpb.PowerRentalReq{
+		GoodID:               &goodID,
+		DeviceTypeID:         &deviceTypeID,
+		VendorLocationID:     &locationID,
+		UnitPrice:            func() *string { s := decimal.NewFromInt(120).String(); return &s }(),
+		QuantityUnit:         func() *string { s := "TiB"; return &s }(),
+		QuantityUnitAmount:   func() *string { s := decimal.NewFromInt(120).String(); return &s }(),
+		DeliveryAt:           func() *uint32 { u := uint32(time.Now().Unix()); return &u }(),
+		GoodType:             func() *types.GoodType { e := types.GoodType_PowerRental; return &e }(),
+		BenefitType:          func() *types.BenefitType { e := types.BenefitType_BenefitTypePlatform; return &e }(),
+		Name:                 &ret.GoodName,
+		ServiceStartAt:       func() *uint32 { u := uint32(time.Now().Unix()); return &u }(),
+		StartMode:            func() *types.GoodStartMode { e := types.GoodStartMode_GoodStartModeInstantly; return &e }(),
+		TestOnly:             func() *bool { b := true; return &b }(),
+		BenefitIntervalHours: func() *uint32 { u := uint32(24); return &u }(),
+		StockMode:            func() *types.GoodStockMode { e := types.GoodStockMode_GoodStockByUnique; return &e }(),
+		Total:                func() *string { s := decimal.NewFromInt(120).String(); return &s }(),
+	})
+	assert.Nil(t, err)
+
+	err = apppowerrental1.CreatePowerRental(context.Background(), &apppowerrentalmwpb.PowerRentalReq{
+		AppID:          &ret.AppID,
+		GoodID:         &goodID,
+		AppGoodID:      &ret.AppGoodID,
+		Name:           &ret.GoodName,
+		ServiceStartAt: func() *uint32 { u := uint32(time.Now().Unix()); return &u }(),
+		UnitPrice:      func() *string { s := decimal.NewFromInt(120).String(); return &s }(),
+		SaleMode:       func() *types.GoodSaleMode { e := types.GoodSaleMode_GoodSaleModeMainnetSpot; return &e }(),
 	})
 	assert.Nil(t, err)
 
 	return func(*testing.T) {
-		_, _ = good1.DeleteGood(context.Background(), good.ID)
-		_, _ = devicetype1.DeleteDeviceInfo(context.Background(), device.ID)
-		_, _ = vendorlocation1.DeleteLocation(context.Background(), vendorLocation.ID)
-		_, _ = vendorbrand1.DeleteBrand(context.Background(), info1.ID)
+		_ = apppowerrental1.DeletePowerRental(context.Background(), nil, nil, &ret.AppGoodID)
+		_ = powerrental1.DeletePowerRental(context.Background(), nil, nil, &goodID)
+		_ = location1.DeleteLocation(context.Background(), nil, &locationID)
+		_ = brand1.DeleteBrand(context.Background(), nil, &brandID)
+		_ = devicetype1.DeleteDeviceType(context.Background(), nil, &deviceTypeID)
+		_ = manufacturer1.DeleteManufacturer(context.Background(), nil, &manufacturerID)
 	}
 }
 
 func createRecommend(t *testing.T) {
-	info, err := CreateRecommend(context.Background(), &npool.RecommendReq{
+	err := CreateRecommend(context.Background(), &npool.RecommendReq{
 		EntID:          &ret.EntID,
-		AppID:          &ret.AppID,
 		RecommenderID:  &ret.RecommenderID,
-		GoodID:         &ret.GoodID,
+		AppGoodID:      &ret.AppGoodID,
 		Message:        &ret.Message,
 		RecommendIndex: &ret.RecommendIndex,
 	})
 	if assert.Nil(t, err) {
-		ret.CreatedAt = info.CreatedAt
-		ret.UpdatedAt = info.UpdatedAt
-		ret.ID = info.ID
-		assert.Equal(t, &ret, info)
+		info, err := GetRecommend(context.Background(), ret.EntID)
+		if assert.Nil(t, err) {
+			ret.CreatedAt = info.CreatedAt
+			ret.UpdatedAt = info.UpdatedAt
+			ret.ID = info.ID
+			assert.Equal(t, &ret, info)
+		}
 	}
 }
 
 func updateRecommend(t *testing.T) {
-	ret.Message = uuid.NewString()
-	info, err := UpdateRecommend(context.Background(), &npool.RecommendReq{
+	err := UpdateRecommend(context.Background(), &npool.RecommendReq{
 		ID:      &ret.ID,
 		Message: &ret.Message,
 	})
 	if assert.Nil(t, err) {
-		ret.UpdatedAt = info.UpdatedAt
-		assert.Equal(t, &ret, info)
+		info, err := GetRecommend(context.Background(), ret.EntID)
+		if assert.Nil(t, err) {
+			ret.UpdatedAt = info.UpdatedAt
+			assert.Equal(t, &ret, info)
+		}
 	}
 }
 
+//nolint
 func getRecommends(t *testing.T) {
 	infos, total, err := GetRecommends(context.Background(), &npool.Conds{
 		ID:            &basetypes.Uint32Val{Op: cruder.EQ, Value: ret.ID},
 		EntID:         &basetypes.StringVal{Op: cruder.EQ, Value: ret.EntID},
 		AppID:         &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppID},
 		RecommenderID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.RecommenderID},
-		GoodID:        &basetypes.StringVal{Op: cruder.EQ, Value: ret.GoodID},
-		GoodIDs:       &basetypes.StringSliceVal{Op: cruder.IN, Value: []string{ret.GoodID}},
+		AppGoodID:     &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppGoodID},
+		AppGoodIDs:    &basetypes.StringSliceVal{Op: cruder.IN, Value: []string{ret.AppGoodID}},
 	}, int32(0), int32(2))
 	if assert.Nil(t, err) {
 		if assert.Equal(t, uint32(1), total) {
@@ -195,33 +187,33 @@ func getRecommends(t *testing.T) {
 	}
 }
 
+//nolint
 func getRecommendOnly(t *testing.T) {
 	info, err := GetRecommendOnly(context.Background(), &npool.Conds{
 		ID:            &basetypes.Uint32Val{Op: cruder.EQ, Value: ret.ID},
 		EntID:         &basetypes.StringVal{Op: cruder.EQ, Value: ret.EntID},
 		AppID:         &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppID},
 		RecommenderID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.RecommenderID},
-		GoodID:        &basetypes.StringVal{Op: cruder.EQ, Value: ret.GoodID},
-		GoodIDs:       &basetypes.StringSliceVal{Op: cruder.IN, Value: []string{ret.GoodID}},
+		AppGoodID:     &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppGoodID},
+		AppGoodIDs:    &basetypes.StringSliceVal{Op: cruder.IN, Value: []string{ret.AppGoodID}},
 	})
 	if assert.Nil(t, err) {
 		assert.Equal(t, &ret, info)
 	}
 }
 
+//nolint
 func deleteRecommend(t *testing.T) {
-	info, err := DeleteRecommend(context.Background(), ret.ID)
-	if assert.Nil(t, err) {
-		assert.Equal(t, &ret, info)
-	}
+	err := DeleteRecommend(context.Background(), &ret.ID, &ret.EntID)
+	assert.Nil(t, err)
 
-	info, err = GetRecommendOnly(context.Background(), &npool.Conds{
+	info, err := GetRecommendOnly(context.Background(), &npool.Conds{
 		ID:            &basetypes.Uint32Val{Op: cruder.EQ, Value: ret.ID},
 		EntID:         &basetypes.StringVal{Op: cruder.EQ, Value: ret.EntID},
 		AppID:         &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppID},
 		RecommenderID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.RecommenderID},
-		GoodID:        &basetypes.StringVal{Op: cruder.EQ, Value: ret.GoodID},
-		GoodIDs:       &basetypes.StringSliceVal{Op: cruder.IN, Value: []string{ret.GoodID}},
+		AppGoodID:     &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppGoodID},
+		AppGoodIDs:    &basetypes.StringSliceVal{Op: cruder.IN, Value: []string{ret.AppGoodID}},
 	})
 	assert.Nil(t, err)
 	assert.Nil(t, info)
