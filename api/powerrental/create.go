37a5e7857602aeebcfece49985e3f96f15777a0e
Zhao KK <kikakkz@hotmail.com>
2024-04-07 06:35:24 +0000

    Correct power rental parameter check

--- api/powerrental/create.go
+++ api/powerrental/create.go
@@ -31,23 +31,23 @@ func (s *Server) CreatePowerRental(ctx context.Context, in *npool.CreatePowerRen
 		powerrental1.WithQuantityUnit(req.QuantityUnit, true),
 		powerrental1.WithQuantityUnitAmount(req.QuantityUnitAmount, true),
 		powerrental1.WithDeliveryAt(req.DeliveryAt, true),
-		powerrental1.WithUnitLockDeposit(req.UnitLockDeposit, true),
-		powerrental1.WithDurationType(req.DurationType, true),
+		powerrental1.WithUnitLockDeposit(req.UnitLockDeposit, false),
+		powerrental1.WithDurationType(req.DurationType, false),
 
 		powerrental1.WithGoodType(req.GoodType, true),
 		powerrental1.WithBenefitType(req.BenefitType, true),
 		powerrental1.WithName(req.Name, true),
 		powerrental1.WithServiceStartAt(req.ServiceStartAt, true),
 		powerrental1.WithStartMode(req.StartMode, true),
-		powerrental1.WithTestOnly(req.TestOnly, true),
+		powerrental1.WithTestOnly(req.TestOnly, false),
 		powerrental1.WithBenefitIntervalHours(req.BenefitIntervalHours, true),
-		powerrental1.WithPurchasable(req.Purchasable, true),
-		powerrental1.WithOnline(req.Online, true),
+		powerrental1.WithPurchasable(req.Purchasable, false),
+		powerrental1.WithOnline(req.Online, false),
 
-		powerrental1.WithStockID(req.StockID, true),
+		powerrental1.WithStockID(req.StockID, false),
 		powerrental1.WithTotal(req.Total, true),
 		powerrental1.WithStockMode(req.StockMode, true),
-		powerrental1.WithStocks(req.MiningGoodStocks, true),
+		powerrental1.WithStocks(req.MiningGoodStocks, false),
 	)
 	if err != nil {
 		logger.Sugar().Errorw(
