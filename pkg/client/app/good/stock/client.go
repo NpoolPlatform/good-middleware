57a016d2b3658802bdf4cc1c6f07a3cf8deb105d
Zhao KK <kikakkz@hotmail.com>
2024-04-07 09:46:41 +0000

    Add stock client

--- pkg/client/app/good/stock/client.go
+++ pkg/client/app/good/stock/client.go
@@ -5,113 +5,61 @@ import (
 	"time"
 
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
-
-	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	npool "github.com/NpoolPlatform/message/npool/good/mw/v1/app/good/stock"
-
 	servicename "github.com/NpoolPlatform/good-middleware/pkg/servicename"
+	npool "github.com/NpoolPlatform/message/npool/good/mw/v1/app/good/stock"
+	"google.golang.org/grpc"
 )
 
-var timeout = 10 * time.Second
-
-type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
-
-func do(ctx context.Context, handler handler) (cruder.Any, error) {
-	_ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-
-	conn, err := grpc2.GetGRPCConn(servicename.ServiceDomain, grpc2.GRPCTAG)
-	if err != nil {
-		return nil, err
-	}
-
-	defer conn.Close()
-
-	cli := npool.NewMiddlewareClient(conn)
-
-	return handler(_ctx, cli)
+func withClient(ctx context.Context, handler func(context.Context, npool.MiddlewareClient) (interface{}, error)) (interface{}, error) {
+	return grpc2.WithGRPCConn(
+		ctx,
+		servicename.ServiceDomain,
+		10*time.Second,
+		func(_ctx context.Context, conn *grpc.ClientConn) (interface{}, error) {
+			return handler(_ctx, npool.NewMiddlewareClient(conn))
+		},
+		grpc2.GRPCTAG,
+	)
 }
 
-func LockStock(ctx context.Context, req *npool.LockRequest) (*npool.Stock, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.Lock(ctx, req)
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
+func LockStock(ctx context.Context, req *npool.LockRequest) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.Lock(_ctx, req)
 	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.Stock), nil
+	return err
 }
 
-func UnlockStock(ctx context.Context, req *npool.UnlockRequest) (*npool.Stock, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.Unlock(ctx, req)
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
+func UnlockStock(ctx context.Context, req *npool.UnlockRequest) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.Unlock(_ctx, req)
 	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.Stock), nil
+	return err
 }
 
-func WaitStartStock(ctx context.Context, req *npool.WaitStartRequest) (*npool.Stock, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.WaitStart(ctx, req)
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
+func WaitStartStock(ctx context.Context, req *npool.WaitStartRequest) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.WaitStart(_ctx, req)
 	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.Stock), nil
+	return err
 }
 
-func InServiceStock(ctx context.Context, req *npool.InServiceRequest) (*npool.Stock, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.InService(ctx, req)
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
+func InServiceStock(ctx context.Context, req *npool.InServiceRequest) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.InService(_ctx, req)
 	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.Stock), nil
+	return err
 }
 
-func ChargeBackStock(ctx context.Context, req *npool.ChargeBackRequest) (*npool.Stock, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.ChargeBack(ctx, req)
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
+func ChargeBackStock(ctx context.Context, req *npool.ChargeBackRequest) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.ChargeBack(_ctx, req)
 	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.Stock), nil
+	return err
 }
 
-func ExpireStock(ctx context.Context, req *npool.ExpireRequest) (*npool.Stock, error) {
-	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.Expire(ctx, req)
-		if err != nil {
-			return nil, err
-		}
-		return resp.Info, nil
+func ExpireStock(ctx context.Context, req *npool.ExpireRequest) error {
+	_, err := withClient(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (interface{}, error) {
+		return cli.Expire(_ctx, req)
 	})
-	if err != nil {
-		return nil, err
-	}
-	return info.(*npool.Stock), nil
+	return err
 }
