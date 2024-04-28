40fae73166d9a77c6ecf4239c8072cb0c1c08070
Zhao KK <kikakkz@hotmail.com>
2024-04-28 11:30:00 +0000

    Fix: http://47.116.78.131:10083/issues/3215

--- pkg/mw/app/good/topmost/good/poster/update.go
+++ pkg/mw/app/good/topmost/good/poster/update.go
@@ -2,6 +2,7 @@ package poster
 
 import (
 	"context"
+	"fmt"
 
 	topmostgoodpostercrud "github.com/NpoolPlatform/good-middleware/pkg/crud/app/good/topmost/good/poster"
 	"github.com/NpoolPlatform/good-middleware/pkg/db"
@@ -23,6 +24,15 @@ func (h *updateHandler) updatePoster(ctx context.Context, cli *ent.Client) error
 }
 
 func (h *Handler) UpdatePoster(ctx context.Context) error {
+	info, err := h.GetPoster(ctx)
+	if err != nil {
+		return err
+	}
+	if info == nil {
+		return fmt.Errorf("invalid poster")
+	}
+
+	h.ID = &info.ID
 	handler := &updateHandler{
 		Handler: h,
 	}
