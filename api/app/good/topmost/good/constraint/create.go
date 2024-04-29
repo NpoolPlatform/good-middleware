3ae5487323c3d6a73dd51f4e76a7a8f0fc4065fe
Zhao KK <kikakkz@hotmail.com>
2024-04-29 06:53:32 +0000

    Constraint is must

--- api/app/good/topmost/good/constraint/create.go
+++ api/app/good/topmost/good/constraint/create.go
@@ -25,7 +25,7 @@ func (s *Server) CreateTopMostGoodConstraint(ctx context.Context, in *npool.Crea
 		ctx,
 		constraint1.WithEntID(req.EntID, false),
 		constraint1.WithTopMostGoodID(req.TopMostGoodID, true),
-		constraint1.WithConstraint(req.Constraint, false),
+		constraint1.WithConstraint(req.Constraint, true),
 		constraint1.WithTargetValue(req.TargetValue, true),
 		constraint1.WithIndex(req.Index, false),
 	)
