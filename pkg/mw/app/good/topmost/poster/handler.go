c2652de6afc4ad5d206cd9c13b322670b7937021
Zhao KK <kikakkz@hotmail.com>
2024-05-16 07:19:22 +0000

    Fix: http://47.116.78.131:10083/issues/3188

--- pkg/mw/app/good/topmost/poster/handler.go
+++ pkg/mw/app/good/topmost/poster/handler.go
@@ -89,6 +89,12 @@ func WithTopMostID(id *string, must bool) func(context.Context, *Handler) error 
 
 func WithPoster(s *string, must bool) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
+		if s == nil {
+			if must {
+				return wlog.Errorf("invalid poster")
+			}
+			return nil
+		}
 		h.Poster = s
 		return nil
 	}
