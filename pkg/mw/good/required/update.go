9c49372b1d5c03d671968b3578c2ebc6c2fec10b
Zhao KK <kikakkz@hotmail.com>
2024-05-16 07:51:54 +0000

    Fix: http://47.116.78.131:10083/issues/3050

--- pkg/mw/good/required/update.go
+++ pkg/mw/good/required/update.go
@@ -3,12 +3,22 @@ package required
 import (
 	"context"
 
+	wlog "github.com/NpoolPlatform/go-service-framework/pkg/wlog"
 	requiredcrud "github.com/NpoolPlatform/good-middleware/pkg/crud/good/required"
 	"github.com/NpoolPlatform/good-middleware/pkg/db"
 	"github.com/NpoolPlatform/good-middleware/pkg/db/ent"
 )
 
 func (h *Handler) UpdateRequired(ctx context.Context) error {
+	info, err := h.GetRequired(ctx)
+	if err != nil {
+		return wlog.WrapError(err)
+	}
+	if info == nil {
+		return wlog.Errorf("invalid required")
+	}
+
+	h.ID = &info.ID
 	return db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
 		if _, err := requiredcrud.UpdateSet(
 			cli.RequiredGood.UpdateOneID(*h.ID),
@@ -16,7 +26,7 @@ func (h *Handler) UpdateRequired(ctx context.Context) error {
 				Must: h.Must,
 			},
 		).Save(_ctx); err != nil {
-			return err
+			return wlog.WrapError(err)
 		}
 		return nil
 	})
