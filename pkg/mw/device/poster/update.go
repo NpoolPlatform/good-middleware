178cb76241a3609095a53991481da3005100eff5
Zhao KK <kikakkz@hotmail.com>
2024-05-16 08:05:46 +0000

    Fix: http://47.116.78.131:10083/issues/3012

--- pkg/mw/device/poster/update.go
+++ pkg/mw/device/poster/update.go
@@ -3,6 +3,7 @@ package poster
 import (
 	"context"
 
+	wlog "github.com/NpoolPlatform/go-service-framework/pkg/wlog"
 	devicepostercrud "github.com/NpoolPlatform/good-middleware/pkg/crud/device/poster"
 	"github.com/NpoolPlatform/good-middleware/pkg/db"
 	"github.com/NpoolPlatform/good-middleware/pkg/db/ent"
@@ -17,12 +18,21 @@ func (h *updateHandler) updatePoster(ctx context.Context, cli *ent.Client) error
 		cli.DevicePoster.UpdateOneID(*h.ID),
 		&h.Req,
 	).Save(ctx); err != nil {
-		return err
+		return wlog.WrapError(err)
 	}
 	return nil
 }
 
 func (h *Handler) UpdatePoster(ctx context.Context) error {
+	info, err := h.GetPoster(ctx)
+	if err != nil {
+		return wlog.WrapError(err)
+	}
+	if info == nil {
+		return wlog.Errorf("invalid poster")
+	}
+
+	h.ID = &info.ID
 	handler := &updateHandler{
 		Handler: h,
 	}
