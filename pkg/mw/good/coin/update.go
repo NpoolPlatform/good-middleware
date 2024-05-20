94f4f81fdc9c685495f5b06071782321b0984ea1
Zhao KK <kikakkz@hotmail.com>
2024-05-20 09:18:38 +0000

    Fix: http://47.116.78.131:10083/issues/3447

--- pkg/mw/good/coin/update.go
+++ pkg/mw/good/coin/update.go
@@ -2,23 +2,67 @@ package coin
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	wlog "github.com/NpoolPlatform/go-service-framework/pkg/wlog"
-	goodcoincrud "github.com/NpoolPlatform/good-middleware/pkg/crud/good/coin"
 	"github.com/NpoolPlatform/good-middleware/pkg/db"
 	"github.com/NpoolPlatform/good-middleware/pkg/db/ent"
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 )
 
 type updateHandler struct {
 	*Handler
+	sql    string
+	goodID string
 }
 
-func (h *Handler) updateGoodCoin(ctx context.Context, cli *ent.Client) error {
-	if _, err := goodcoincrud.UpdateSet(
-		cli.GoodCoin.UpdateOneID(*h.ID),
-		&h.Req,
-	).Save(ctx); err != nil {
+func (h *updateHandler) constructSQL() error {
+	if h.ID == nil && h.EntID == nil {
+		return wlog.Errorf("invalid id")
+	}
+
+	set := "set "
+	now := uint32(time.Now().Unix())
+
+	_sql := "update good_coins "
+	if h.Main != nil {
+		_sql += fmt.Sprintf("%vmain = %v, ", set, *h.Main)
+		set = ""
+	}
+	if h.Index != nil {
+		_sql += fmt.Sprintf("%v`index` = %v, ", set, *h.Index)
+		set = ""
+	}
+	if set != "" {
+		return wlog.WrapError(cruder.ErrUpdateNothing)
+	}
+	_sql += fmt.Sprintf("updated_at = %v ", now)
+	whereAnd := "where "
+	if h.ID != nil {
+		_sql += fmt.Sprintf("where id = %v ", *h.ID)
+		whereAnd = "and"
+	}
+	if h.EntID != nil {
+		_sql += fmt.Sprintf("%v ent_id = '%v'", whereAnd, *h.EntID)
+	}
+	if h.Main != nil && *h.Main {
+		_sql += fmt.Sprintf(
+			" and not exist (good_id = '%v' and deleted_at = 0 and main = 1)",
+			h.goodID,
+		)
+	}
+	h.sql = _sql
+	return nil
+}
+
+func (h *updateHandler) updateGoodCoin(ctx context.Context, tx *ent.Tx) error {
+	rc, err := tx.ExecContext(ctx, h.sql)
+	if err != nil {
 		return wlog.WrapError(err)
+	}
+	if n, err := rc.RowsAffected(); err != nil || n != 1 {
+		return wlog.Errorf("fail update goodcoin: %v", err)
 	}
 	return nil
 }
@@ -35,9 +79,12 @@ func (h *Handler) UpdateGoodCoin(ctx context.Context) error {
 	h.ID = &info.ID
 	handler := &updateHandler{
 		Handler: h,
+		goodID:  info.GoodID,
 	}
-
-	return db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		return handler.updateGoodCoin(_ctx, cli)
+	if err := handler.constructSQL(); err != nil {
+		return wlog.WrapError(err)
+	}
+	return db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
+		return handler.updateGoodCoin(_ctx, tx)
 	})
 }
